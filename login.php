523a0abd768f42052ba95e8401df15158768b874
TomEWilkinson <tom-wil-ko@hotmail.co.uk>
2017-01-21 19:40:55 +0000

    removing database login details

--- login.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$dbhost  = 'localhost'; // e.g. 'mudfoot.doc.stu.mmu.ac.uk' 
-	$dbname  = 'wilkinst'; // e.g. 'johnd' 
-	$dbuser  = 'wilkinst'; // e.g. 'johnd' 
-	$dbpass  = 'zenTraly5'; // e.g. 'topsecret' 
-
-?>
